654c0af758ecc972713590504d618103a5f2fe28
rafabrasail <rafaelrosenberg89@gmail.com>
2022-02-15 18:18:53 -0300

    token

--- src/UserLists.js
+++ src/UserLists.js
@@ -2,11 +2,30 @@ import React from "react";
 import ListComponent from "./ListComponent";
 
 
-export default function UserLists(){
-    return(
-        <div>
-            <ListComponent listName={'Minha Lista'} />
-            <ListComponent listName={'Minha Lista 2'} />
-        </div>
-    )
+export default class UserLists extends React.Component{
+    state = {lists: null, loading: true}
+    
+    async componentDidMount(){
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        config.headers['Authorization'] = 'Token 93ecd5b338cb11f82cd9d41f2e3817a1ee564fc5'
+
+        var url = 'http://127.0.0.1:8000/list/';
+        const response = await fetch(url, config);
+        const data = await response.json();
+        console.log(data);
+        this.setState({lists: data, loading: false})
+    }
+
+    render(){
+        return(
+            <div>
+                <ListComponent listName={'Minha Lista'} />
+                <ListComponent listName={'Minha Lista 2'} />
+            </div>
+        )
+    }
 }
