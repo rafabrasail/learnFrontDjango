e712a12bf6730c2e8186cc976435017e072f86d0
rafabrasail <rafaelrosenberg89@gmail.com>
2022-02-15 16:48:36 -0300

    commit02

---
+++ src/ItemComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function ItemComponent(props){
+    return(
+            <li>Item desc.: {props.name}</li>
+    )
+}
---
+++ src/ListComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ItemComponent from "./ItemComponent";
+
+
+export default function ListComponent(props){
+    return (
+        <div>
+            <h2>{props.listName}</h2>
+            <ul>
+                <ItemComponent name={'Meu item'} />
+                <ItemComponent name={'Meu item 2'} />
+            </ul>
+        </div>
+    )
+}
