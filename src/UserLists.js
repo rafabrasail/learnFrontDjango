dc36d56e004fc1b3bd21ba7ef2614e1fa6afacc5
rafabrasail <rafaelrosenberg89@gmail.com>
2022-02-16 17:38:55 -0300

    flow apos login

--- src/UserLists.js
+++ src/UserLists.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ListComponent from "./ListComponent";
-import LoginComponent from "./LoginComponent";
+
 
 
 export default class UserLists extends React.Component{
@@ -23,15 +23,11 @@ export default class UserLists extends React.Component{
 
     render(){
         const listApi = this.state.lists;
-        var token = localStorage.getItem('token')
-
-        if(!token)
-            {return <LoginComponent />}
-        else
-            return(
-                <div>
-                    {listApi.map(list => <ListComponent key={list.id} listName={list.name} items={list.item_set} /> )}
-                </div>
-            )
+        return (
+            <div>
+                {listApi.map(list => <ListComponent key={list.id} listName={list.name} items={list.item_set} /> )}
+            </div>
+        )
     }
+
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import UserLists from './UserLists';
 import reportWebVitals from './reportWebVitals';
 import LoginComponent from './LoginComponent';
 
@@ -9,7 +8,6 @@ import LoginComponent from './LoginComponent';
 ReactDOM.render(
   <React.StrictMode>
     <LoginComponent />
-    <UserLists />
   </React.StrictMode>,
   document.getElementById('root')
 );
